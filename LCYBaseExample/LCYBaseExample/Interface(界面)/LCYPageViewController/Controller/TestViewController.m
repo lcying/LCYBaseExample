c65e974754e4738c9773b2b88d3ad1fb34105d89
刘岑颖 <m18758103483@163.com>
2018-01-24 13:58:42 +0800

    添加了显示评分星星的工具

--- LCYBaseExample/LCYBaseExample/Interface(界面)/LCYPageViewController/Controller/TestViewController.m
+++ LCYBaseExample/LCYBaseExample/Interface(界面)/LCYPageViewController/Controller/TestViewController.m
@@ -8,8 +8,11 @@
 
 #import "TestViewController.h"
 #import "SettingViewController.h"
+#import "HCSStarRatingView.h"
 
 @interface TestViewController ()
+
+@property (nonatomic, strong) HCSStarRatingView *starView;
 
 @end
 
@@ -23,11 +26,32 @@
     [button addTarget:self action:@selector(buttonClickedAction) forControlEvents:UIControlEventTouchUpInside];
     [button setTitle:@"点击跳转" forState:UIControlStateNormal];
     
+    
+    [self starView];
 }
 
 - (void)buttonClickedAction{
     SettingViewController *vc = [[SettingViewController alloc] init];
     [self.navigationController pushViewController:vc animated:YES];
+}
+
+- (HCSStarRatingView *)starView{
+    if (!_starView) {
+        _starView = [[HCSStarRatingView alloc] initWithFrame:CGRectMake(20, 70, SCREEN_WIDTH - 40, 50)];
+        [self.view addSubview:_starView];
+        _starView.maximumValue = 5;//最多星星数
+        _starView.minimumValue = 0.5;//最少选中星星数
+        _starView.allowsHalfStars = YES;//是否允许半颗星星显示
+        _starView.accurateHalfStars = YES;//是否连续显示星星
+//        _starView.continuous = YES;
+        _starView.starBorderColor = [UIColor redColor];//星星边框颜色
+        _starView.starBorderWidth = 1.0;//星星边框宽度
+        _starView.emptyStarColor = [UIColor lightGrayColor];//未选中星星的填充色
+        _starView.tintColor = [UIColor greenColor];//选中星星的填充色
+        _starView.backgroundColor = [UIColor clearColor];
+        _starView.value = 4.0;//当前值
+    }
+    return _starView;
 }
 
 @end
